72acca43a488ab8a595d0ed196b13ed10bf6f44e
nsburtsev <burtsev.ns@gmail.com>
2023-09-13 18:45:50 +0600

    Initial Commit

---
+++ Password/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Password
+//
+//  Created by Нюргун on 13.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
